ae3d30096d3fafc47ef7aa738162c5ce05ca315f
Erik Larson <eriklarsonco@gmail.com>
2024-07-29 13:15:36 -0400

    updated landing page

--- classes/theme/class-font.php
+++ classes/theme/class-font.php
@@ -14,11 +14,23 @@ class FontFaceConfig
         <style>
             @font-face {
                 font-family: 'Roboto Condensed';
-                font-weight: 300 400 500;
+                font-weight: 300;
                 font-display: auto;
-                src: url(<?php echo $path . '/RobotoCondensed-Light.ttf'; ?>) format('truetype'),
-                     url(<?php echo $path . '/RobotoCondensed-Regular.ttf'; ?>) format('truetype'),
-                     url(<?php echo $path . '/RobotoCondensed-Medium.ttf'; ?>) format('truetype');
+                src: url(<?php echo $path . '/RobotoCondensed-Light.ttf'; ?>) format('truetype');
+            }
+
+            @font-face {
+                font-family: 'Roboto Condensed';
+                font-weight: 400;
+                font-display: auto;
+                src: url(<?php echo $path . '/RobotoCondensed-Regular.ttf'; ?>) format('truetype');
+            }
+
+            @font-face {
+                font-family: 'Roboto Condensed';
+                font-weight: 500;
+                font-display: auto;
+                src: url(<?php echo $path . '/RobotoCondensed-Medium.ttf'; ?>) format('truetype');
             }
         </style>
         <?php
--- theme/index.php
+++ theme/index.php
@@ -2,7 +2,9 @@
 
 <div class="flex items-center mb-5">
     <div class="flex-1">
-        <span class="uppercase tracking-widest font-normal text-sm">Mercury Theme</span>
+        <span class="relative uppercase tracking-widest font-normal text-sm before:absolute before:content-[''] md:before:bottom-[calc(100%_+_5rem)] before:bottom-[calc(100%_+_30px)] before:right-[2px] before:w-[2px] before:h-screen before:bg-slate-700 before:rounded-full">
+            Mercury Theme
+        </span>
     </div>
 
     <div>
@@ -10,7 +12,9 @@
     </div>
 </div>
 
-<h1 class="mb-1"><?php bloginfo('name'); ?></h1>
+<h1 class="relative mb-1 before:absolute before:content-[''] before:top-[calc(50%_-_1px)] md:before:right-[calc(100%_+_5rem)] before:right-[calc(100%_+_15px)] before:w-screen before:h-[2px] before:bg-slate-700 before:rounded-full">
+    <?php bloginfo('name'); ?>
+</h1>
 <?php if (get_bloginfo('description')): ?>
     <span class="block leading-7"><?php bloginfo('description'); ?></span>
 <?php else: ?>
@@ -21,13 +25,13 @@
     <div class="flex-1">
         <a href="<?php echo '#'; ?>"
            target="_blank"
-           class="tracking-widest font-normal text-accent">View Website</a>
+           class="uppercase tracking-widest font-normal text-sm text-accent">View Website</a>
     </div>
 
-    <div>
+    <div class="md:block hidden">
         <a href="https://github.com/eriklarsondev/project-mercury"
            target="_blank"
-           class="tracking-widest font-normal"><i class="fab fa-github"></i> Documentation</a>
+           class="uppercase tracking-widest font-normal text-sm"><i class="fab fa-github"></i> Documentation</a>
     </div>
 </div>
 
